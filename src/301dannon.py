c06cb09023c358a185883b94a82c2fd29c09fafa
Paul MARLIERE <paul.marliere@epitech.eu>
2021-10-04 10:08:29 +0200

    [ADD] QuickSort, merge Sort, Insertion sort and some error handling

--- src/301dannon.py
+++ src/301dannon.py
@@ -2,38 +2,58 @@
 # -*- coding: utf-8 -*-
 
 import sys
-import random
+import copy
+import os
 
-comp = 0
+class comp:
+    def __init__(self):
+        self.staticvariable = 0
+    def reset(self):
+        self.staticvariable = 0
+    def increment(self):
+        self.staticvariable += 1
+    def getComp(self):
+        return(self.staticvariable)
 
-#file to list of numbers
+def usage():
+    print("USAGE\n\t./301dannon file\nDESCRIPTION\n\tfile\tfile that contains the numbers to be sorted, separated by space")
+    exit(0)
+
 def parse(txt):
+    if not os.path.isfile(txt):
+        exit(84)
     f = open(txt, "r")
     buffer = f.read()
     buffer =  buffer.replace("\n", " ")
     buffer =  buffer.replace("\t", " ")
     numbers = buffer.split(" ")
+    for i in range(len(numbers)):
+        if numbers[i] == '':
+            del numbers[i]
+    for i in numbers:
+        for j in i:
+            if j not in "0123456789- .":
+                exit(84)
     result = list(map(float, numbers))
     return result
 
-def bubble_sort(nb_lst):
+def bubble_sort(nb_lst, comparisons):
     for j in range(len(nb_lst), 0, -1):
         for i in range(0, j - 1):
-            globals()['comp'] += 1
+            comparisons.increment()
             if nb_lst[i] > nb_lst[i + 1]:
                 nb_temp = nb_lst[i + 1]
                 nb_lst[i + 1] = nb_lst[i]
                 nb_lst[i] = nb_temp
-    #print("Bubble result:", nb_lst)
     return nb_lst
 
-def select(nb_lst):
+def select(nb_lst, comparisons):
     offset = 0
     while offset + 1 != len(nb_lst):
         smallest = nb_lst[offset]
         smallest_i = offset
         for i in range(offset + 1, len(nb_lst)):
-            globals()['comp'] += 1
+            comparisons.increment()
             if nb_lst[i] < smallest:
                 smallest = nb_lst[i]
                 smallest_i = i
@@ -43,53 +63,96 @@ def select(nb_lst):
         offset += 1
     return nb_lst
 
-#Pas encore bon / copié
-def insertion(arr):
-    for i in range(1, len(arr)):
-        key = arr[i]
-        j = i-1
-        globals()['comp'] += 1
-        while j >=0 and key < arr[j] :
-            arr[j+1] = arr[j]
-            j -= 1
-            if key < arr[j]:
-                globals()['comp'] += 1
-        arr[j+1] = key 
+def insertion(nb_lst, comparisons):
+    for i in range(1, len(nb_lst)):
+        nb_lst.insert(0, nb_lst[i])
+        nb_lst.pop(i + 1)
+        j = 0
+        if j < i:
+            comparisons.increment()
+        while j < i and nb_lst[j + 1] <= nb_lst[j]:
+            nb_lst[j], nb_lst[j + 1] = nb_lst[j + 1], nb_lst[j]
+            j = j + 1
+            if j < i:
+                comparisons.increment()
+    return(nb_lst)
 
-def quick_part(nb_lst, first, last, pivot):
-    nb_lst[pivot], nb_lst[last] = nb_lst[last], nb_lst[pivot]
-    j = first
-    for i in range(first, last):
-        globals()['comp'] += 1
-        if nb_lst[i] <= nb_lst[last]:
-            nb_lst[i], nb_lst[j] = nb_lst[j], nb_lst[i]
-            j += 1
-    nb_lst[last], nb_lst[j] = nb_lst[j], nb_lst[last]
-    return j
+def partition(nb_lst, pivot, comparisons):
+    first = []
+    second = []
+    for i in range(1, len(nb_lst)):
+        comparisons.increment()
+        if nb_lst[i] < pivot:
+            first.append(nb_lst[i])
+        else:
+            second.append(nb_lst[i])
+    return first, second
 
-#Pas encore bon
-def quick_sort(nb_lst, first, last):
-    if first < last:
-        pivot = first
-        pivot = quick_part(nb_lst, first, last, pivot)
-        quick_sort(nb_lst, first, pivot-1)
-        quick_sort(nb_lst, pivot + 1, last)
+def quick_sort(nb_lst, comparisons):
+    if len(nb_lst) > 1:
+        part = partition(nb_lst, nb_lst[0], comparisons)
+        rl = quick_sort(part[0], comparisons)
+        rr = quick_sort(part[1], comparisons)
+        nb_lst = rl + [nb_lst[0]] + rr
     return nb_lst
 
-if len(sys.argv) != 2:
-    exit(84)
-numbers = parse(sys.argv[1])
-comp = 0
-selectres = select(numbers.copy())
-print("Selection sort:", comp, "comparisons")
-comp = 0
-bubble = bubble_sort(numbers.copy())
-print("Bubble sort:", comp, "comparisons")
-comp = 0
-insertionres = insertion(numbers.copy())
-print("Insertion sort:", comp, "comparisons")
-comp = 0
-quick = quick_sort(numbers.copy(), 0, len(numbers) - 1)
-print(quick)
-print("Quicksort:", comp, "comparisons")
-exit(0)
+def merging(left, right, comparisons):
+    temp = []
+    i = 0
+    j = 0
+    while i < len(left) and j < len(right):
+        comparisons.increment()
+        if left[i] <= right[j]:
+            temp.append(left[i])
+            i += 1
+        else:
+            temp.append(right[j])
+            j += 1
+    while i < len(left):
+        temp.append(left[i])
+        i += 1
+    while j < len(right):
+        temp.append(right[j])
+        j += 1
+    return temp
+
+def mergeSort(nb_lst, comparisons):
+    if len(nb_lst) > 1:
+        mid = int(len(nb_lst) / 2)
+        start = mergeSort(nb_lst[:mid], comparisons)
+        end = mergeSort(nb_lst[mid:], comparisons)
+        return merging(start, end, comparisons)
+    return nb_lst
+
+def main(ac, av):
+    if ac != 2:
+        exit(84)
+    if av[1] == "-h":
+        usage()
+    numbers = parse(av[1])
+    if len(numbers) <= 1:
+        return
+    comparisons = comp()
+    #select sort
+    select(copy.deepcopy(numbers), comparisons)
+    print('Selection sort:', comparisons.getComp(), "comparisons")
+    #insertion sort
+    comparisons.reset()
+    insertion(copy.deepcopy(numbers), comparisons)
+    print("Insertion sort:", comparisons.getComp(), "comparisons")
+    #bubble sort
+    comparisons.reset()
+    bubble_sort(copy.deepcopy(numbers), comparisons)
+    print ("Bubble sort:", comparisons.getComp(), "comparisons")
+    #quicksort
+    comparisons.reset()
+    quick_sort(copy.deepcopy(numbers), comparisons)
+    print("Quicksort:", comparisons.getComp(), "comparisons")
+    #merge sort
+    comparisons.reset()
+    mergeSort(copy.deepcopy(numbers), comparisons)
+    print("Merge sort:", comparisons.getComp(), "comparisons")
+
+if __name__ == "__main__":
+    main(len(sys.argv) ,sys.argv)
+    exit(0)
